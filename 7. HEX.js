7df938c32d1e57ad5cec8499b78f2e53a8b2aded
ELMIR TANEV <51314768+charlestanev@users.noreply.github.com>
2021-06-20 09:53:48 +0300

    Update 7. HEX.js

--- 7. HEX.js
+++ 7. HEX.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // Write a class Hex, having the following functionality:
 // •	The constructor takes one parameter value, which is a number
 // •	valueOf() This function should return the value property of the hex class.
@@ -14,37 +13,6 @@ class HEX {
 
     valueOf(){
         return this.value
-=======
-// Create a class Stringer, which holds single string and a length property. The class should be initialized with a string, and an initial length. The class should always keep the initial state of its given string.
-// Name the two properties innerString and innerLength.
-// There should also be functionality for increasing and decreasing the initial length property.
-// Implement function increase(length) and decrease(length), which manipulate the length property with the given value.
-// The length property is a numeric value and should not fall below 0. It should not throw any errors, but if an attempt to decrease it below 0 is done, it should be automatically set to 0.
-// You should also implement functionality for toString() function, which returns the string, the object was initialized with. If the length of the string is greater than the length property, the string should be cut to from right to left, so that it has the same length as the length property, and you should add 3 dots after it, if such truncation was done.
-// If the length property is 0, just return 3 dots.
-// Examples
-// lengthLimit.js
-// let test = new Stringer("Test", 5);
-// console.log(test.toString()); // Test
-// test.decrease(3);
-// console.log(test.toString()); // Te...
-// test.decrease(5);
-// console.log(test.toString()); // ...
-// test.increase(4); 
-// console.log(test.toString()); // Test
-// Hints
-// Store the initial string in a property, and do not change it. Upon calling the toString() function, truncate it to the desired value and return it.
-// Submit your solution as a class representation only! No need for IIFEs or wrapping of classes.
-
-class Stringer {
-    constructor(string, length) {
-        this.innerString = string;
-        this.innerLength = length;
-    }
-
-    increase(length) {
-        this.innerLength += length;
->>>>>>> Stashed changes
     }
 
     plus(number){
@@ -53,23 +21,11 @@ class Stringer {
         )
     }
 
-<<<<<<< Updated upstream
     minus(number){
         return new HEX (
             this.value - number
         )
     }
-=======
-    toString() {
-        return this.innerLength === 0 ?
-            '...' :
-            this.innerString.slice(0, this.innerLength) + '...'
-    }
-}
-
-let test = new Stringer("Test", 5);
-console.log(test.toString()); // Test
->>>>>>> Stashed changes
 
     toString() {
         return '0x' + this.value.toString(16).toUpperCase()
@@ -80,7 +36,6 @@ console.log(test.toString()); // Test
     }
 }
 
-<<<<<<< Updated upstream
 let FF = new Hex(255);
 console.log(FF.toString());
 FF.valueOf() + 1 == 256;
@@ -93,7 +48,3 @@ console.log(a.plus(b).toString()==='0xF');
 // 0xFF
 // 0xF
 // true
-=======
-test.increase(4);
-console.log(test.toString()); // Test
->>>>>>> Stashed changes
